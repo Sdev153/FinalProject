077dc456788c889c8c147986fee9ecfd5d360423
jfisher67 <NO@EMAIL.com>
2017-04-20 10:49:44 -0400

    Added sql support to login and register
    Now uses some php and javascript

--- login.php
+++ login.php
@@ -1,5 +1,33 @@
 <!--Final Project SDEV153-->
 
+
+<?php
+/* 
+	 This comment is inside the php tag so it isnt visible to the public 
+	 php stuff for mysql database connetion starts here 
+	 This is probebly really insecure, but it works for this project 
+*/
+session_start();
+require_once('connect.php'); // The file being refrenced contains the username and pass for MySql.. No not the root, I'm not that stupid....
+if(isset($_POST) & !empty($_POST)){
+	$username = mysqli_real_escape_string($connection, $_POST['username']);
+	$password = md5($_POST['password']);
+
+	$sql = "SELECT * FROM `users` WHERE username='$username' AND password='$password'";
+	$result = mysqli_query($connection, $sql);
+	$count = mysqli_num_rows($result);
+	if($count == 1){
+		$_SESSION['username'] = $username;
+	}else{
+		$fmsg = "Invalid Username/Password";
+	}
+}
+if(isset($_SESSION['username'])){
+	$smsg = "User already logged in";
+	
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -24,8 +52,8 @@
             <p><a href="chamber.html">Chamber</a></p>
             <p><a href="business.html">Business</a></p>
 			<p><a href="mobile.html">MOBILE</a></p>
-			<p><a href="join.html">Join</a></p>
-			<p><a href="login.html">Login</a></p>
+			<p><a href="register.php">Join</a></p>
+			<p><a href="login.php">Login</a></p>
 			<p><a href="members.html">Members</a></p>
             <p><a href="contact.html">Contact</a></p>
          </nav>
@@ -54,28 +82,28 @@
 	<div class="articlecontainer">
 		 <header>
 			<h1><img src="images/BGheaderv2.jpg" alt="Placeholder" width="590" height="160"></h1><!--header image-->
+			<h2>Crawfordsville & Montgomery County Chamber<br>Member Login</h2>
 		</header>
 		
 		<article>
-			<h1>Crawfordsville & Montgomery County Chamber<br>Member Login</h1>
-
-			<p>
-				 <form">
-			   <fieldset class="logininfo">
-			      <legend>Member Log-In</legend>
-				  <label for="nameinput">
-				     Username:
-					 <input type="text" name="name" id="nameinput" placeholder="Username">
-				  </label>
-				  <br>
-				   <label for="emailinput">
-				     Password:
-					 <input type="text" name="password" id="passwordinput" placeholder="Password">
-				  </label>
-			   </fieldset>
-		</form>
-		 <button type="button">Submit</button> 
-			</p>
+			
+<div class="container">
+      <?php if(isset($smsg)){ ?><div class="alert alert-success" role="alert"> <?php echo $smsg; ?> </div><?php } ?>
+      <?php if(isset($fmsg)){ ?><div class="alert alert-danger" role="alert"> <?php echo $fmsg; ?> </div><?php } ?>
+      <form class="form-signin" method="POST">
+        <!--<h2 class="form-signin-heading">Please Log-In</h2>-->
+		<fieldset>
+		 <legend>Member Log-In</legend>
+        <div class="input-group">
+		  <span class="input-group-addon" id="basic-addon1"></span>
+		  <input type="text" name="username" class="form-control" placeholder="Username" required>
+		</div>
+        <label for="inputPassword" class="sr-only"></label>
+        <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required>
+		</fieldset>
+        <br><button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
+		<input type="button" onclick="location.href='register.php';" value="Register" <!-- This line is javascript -->
+      </form>
 		
 	</div>
 	<!--end div article container -->
---
+++ register.php
@@ -0,0 +1,108 @@
+<!--Final Project SDEV153-->
+
+<?php
+/* 
+	 This comment is inside the php tag so it isnt visible to the public 
+	 php stuff for mysql database connetion starts here 
+	 This is probebly really insecure, but it works for this project 
+*/
+require_once('connect.php'); // The file being refrenced contains the username and pass for MySql.. No not the root, I'm not that stupid....
+if(isset($_POST) & !empty($_POST)){
+	$username = mysqli_real_escape_string($connection, $_POST['username']);
+	$email = mysqli_real_escape_string($connection, $_POST['email']);
+	$password = md5($_POST['password']);
+
+	$sql = "INSERT INTO `users` (username, email, password) VALUES ('$username', '$email', '$password')";
+	$result = mysqli_query($connection, $sql);
+	if($result){
+		$smsg = "User Registration successfull";
+		//header("Location: login.php");
+	}else{
+		$fmsg = "User registration failed";
+		header("Location: error.php");
+	}
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title>Crawfordsville | Montgomery County Chamber of Commerce</title>
+	<meta charset="utf-8">
+      <meta name="viewport" content="width=device-width">
+	  <link rel="stylesheet" href="styles.css">  
+</head>
+
+<body>
+	<!--begin div nav container -->
+  <div class="navcontainer">
+		<!--begin nav main -->
+         <nav class="sitenavigation">
+            <p><a href="indexMain.html">Home</a></p>
+            <p><a href="directory.html">Directory</a></p>
+            <p><a href="news.html">News</a></p>
+            <p><a href="events.html">Events</a></p>
+			<p><a href="time_capsule.html">Time Capsule</a></p>
+            <p><a href="community.html">Community</a></p>
+            <p><a href="chamber.html">Chamber</a></p>
+            <p><a href="business.html">Business</a></p>
+			<p><a href="mobile.html">MOBILE</a></p>
+			<p><a href="register.php">Join</a></p>
+			<p><a href="login.php">Login</a></p>
+			<p><a href="members.html">Members</a></p>
+            <p><a href="contact.html">Contact</a></p>
+         </nav>
+		 <!--end nav main -->
+		 
+		 <!--begin nav community -->
+		 <nav class="communitynavigation">
+			<p>Our Communities</p>
+            <p><a href="indexMain.html">Alamo</a></p>
+            <p><a href="directory.html">Crawfordsville</a></p>
+            <p><a href="news.html">Darlington</a></p>
+            <p><a href="events.html">Ladoga</a></p>
+			<p><a href="time_capsule.html">Linden</a></p>
+            <p><a href="community.html">New Market</a></p>
+            <p><a href="chamber.html">New Richmond</a></p>
+            <p><a href="business.html">New Ross</a></p>
+			<p><a href="members.html">Waveland</a></p>
+            <p><a href="contact.html">Waynetown</a></p>
+			<p><a href="contact.html">Wingate</a></p>
+         </nav>
+		 <!--end nav community -->
+	</div>
+	<!--end div nav container -->
+	
+	<!--begin div article container -->
+	<div class="articlecontainer">
+		 <header>
+			<h1><img src="images/BGheaderv2.jpg" alt="Placeholder" width="590" height="160"></h1><!--header image-->
+			<h2>Crawfordsville & Montgomery County Chamber<br>New Member Registration</h2>
+		</header>
+		
+		<article>
+			
+<div class="container">
+      <?php if(isset($smsg)){ ?><div class="alert alert-success" role="alert"> <?php echo $smsg; ?> </div><?php } ?>
+      <?php if(isset($fmsg)){ ?><div class="alert alert-danger" role="alert"> <?php echo $fmsg; ?> </div><?php } ?>
+      <form class="form-signin" method="POST">
+        <!--<h2 class="form-signin-heading">Please Register</h2>-->
+		<fieldset>
+		<legend>New Member Registration</legend>
+        <div class="input-group">
+		  <span class="input-group-addon" id="basic-addon1"></span>
+		  <input type="text" name="username" class="form-control" placeholder="Username" required>
+		</div>
+        <label for="inputEmail" class="sr-only"></label>
+        <input type="email" name="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+        <label for="inputPassword" class="sr-only"></label>
+        <br><input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required>
+        <br><button class="btn btn-lg btn-primary btn-block" type="submit">Register</button>
+        <input type="button" onclick="location.href='login.php';" value="Login" <!-- This line is javascript -->
+		</fieldset>
+      </form>
+</div>
+	<!--end div article container -->
